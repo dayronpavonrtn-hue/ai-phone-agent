cbf8a36b5ffc987405b6c8cfa752d24622157577
dayronpavonrtn-hue <dayronpavonrtn@gmail.com>
2026-08-25 16:33:43 -0400

    Finalize V3 with custom app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.dayron.aiphoneagent"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.3.1"
     }
 
     buildTypes {
